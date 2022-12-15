c0c80cd7da1e25f67e549c46752c3d02711e1db6
NaisiG <garkan320286@gmail.com>
2022-12-16 04:25:00 +0800

    Task07

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -4,3 +4,12 @@
 // 456 -> 6
 // 782 -> 2
 // 918 -> 8
+
+Console.Write("Введите трехзначные числа: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number <= 999)
+    Console.Write($"Последняя цифра в числе {number} равна {number % 10}");
+
+else
+    Console.Write("Вы ввели трехзначное число ");
